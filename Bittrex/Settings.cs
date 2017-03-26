8c29d642b7e7235da09d7718522b26c5682f4fa5
eminyuce@gmail.com <eminyuce@gmail.com>
2017-03-26 15:50:03 -0400

    Settings

---
+++ Bittrex/Settings.cs
@@ -0,0 +1,69 @@
+﻿using Bittrex.Helpers;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bittrex
+{
+    public class Settings
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private static void WriteLog(string configName, object defaultValue)
+        {
+            if (!ConfigurationManager.AppSettings.AllKeys.Any(r => r.Equals(configName, StringComparison.InvariantCultureIgnoreCase)))
+            {
+                Logger.Info(string.Format("Config Name {0} is using default value {1}      <add key=\"{0}\" value=\"{1}\" />", configName, defaultValue));
+            }
+        }
+        public static string GetConfigString(string configName, string defaultValue = "")
+        {
+            var appValue = ConfigurationManager.AppSettings[configName];
+            if (string.IsNullOrEmpty(appValue))
+            {
+                WriteLog(configName, defaultValue);
+                return defaultValue;
+            }
+            else
+            {
+                return appValue;
+            }
+        }
+
+        public static bool GetConfigBool(string configName, bool defaultValue = false)
+        {
+
+            var configValue = defaultValue;
+            if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings[configName]))
+            {
+                configValue = ConfigurationManager.AppSettings[configName].ToBool();
+            }
+            else
+            {
+                WriteLog(configName, defaultValue);
+            }
+            return configValue;
+
+        }
+
+        public static int GetConfigInt(string configName, int defaultValue = 0)
+        {
+            int configValue = -1;
+            if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings[configName]))
+            {
+                configValue = ConfigurationManager.AppSettings[configName].ToInt();
+            }
+            else
+            {
+                WriteLog(configName, defaultValue);
+            }
+            return configValue == -1 ? defaultValue : configValue;
+        }
+
+
+    }
+}
