590488d3cf8884559530ac10f9dc17b858a3817c
eminyuce@gmail.com <eminyuce@gmail.com>
2017-03-26 15:41:33 -0400

    Add project files.

---
+++ BittrexApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Bittrex;
+using Bittrex.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BittrexApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ex = new Exchange();
+            ex.Initialise(new ExchangeContext() {
+                ApiKey = "",
+                Secret = "",
+                Simulate = false,
+                QuoteCurrency = "BTC"
+            });
+
+            var history = ex.GetMarketHistory("ETC",50);
+            var dt=  DataTableHelper.ToDataTable(history);
+            
+            DataTableHelper.ToPrintConsole(dt);
+            Console.WriteLine("Type anything to close it.");
+            Console.Read();
+        }
+    }
+}
